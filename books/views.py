27324969d6651452b65dccfa9f3e5515e8015498
emachieng <emillyachieng@gmail.com>
2019-03-25 22:09:00 +0900

    customized forms

--- books/views.py
+++ books/views.py
@@ -18,19 +18,20 @@ def about_page(request):
 	return render(request, "home_page.html", context)
 def contact_page(request):
 	#instance of the form
-	contact_form = ContactForm()
+	contact_form = ContactForm(request.POST or None)
 	context= {
 	"title" : "Contact",
-	"content": "Welcome To The Contact Page."
+	"content": "Welcome To The Contact Page.",
 	"form" : contact_form
 	}
 	
-
-	if request =="POST":
-	#print(request.POST)
-		print(request.POST.get('fullname'))
-		print(request.POST.get('email'))
-		print(request.POST.get('content'))
+	if contact_form.is_valid():
+		print(contact_form.cleaned_data)
+	# if request =="POST":
+	# #print(request.POST)
+	# 	print(request.POST.get('fullname'))
+	# 	print(request.POST.get('email'))
+	# 	print(request.POST.get('content'))
 	return render(request, "contact/view.html", context)
 
 
